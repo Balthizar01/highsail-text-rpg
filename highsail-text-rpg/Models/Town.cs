e3c148a23848e8bd878dd772768142efe1f6c1f7
thefool309 <themagician309@gmail.com>
2024-03-19 17:17:46 -0500

    Created Town class

--- highsail-text-rpg/Models/Town.cs
+++ highsail-text-rpg/Models/Town.cs
@@ -8,5 +8,47 @@ namespace highsail_text_rpg.Models
 {
     internal class Town
     {
+        string name;
+        string description;
+
+        List<Building> buildings;
+
+        
+        //constructors
+        public Town(string name, string description, List<Building> buildings)
+        {
+            this.name = name;
+            this.description = description;
+            this.buildings = buildings;
+        }
+
+        public Town(string name, string description, Building []buildings)
+        {
+            this.name = name;
+            this.description = description;
+            this.buildings = buildings.ToList<Building>();
+        }
+        //accessors
+
+        public string Name() {  return name; }
+        public string Description() { return description; }
+        public List<Building> Buildings() {  return buildings; }
+        //mutators
+
+        public void Name(string newName) { this.name = newName; }
+        public void Description(string newDescription) { this.description = newDescription; }
+
+        void RemoveBuilding(Building building)
+        {
+            buildings.Remove(building);
+        }
+
+        void AddBuildings(Building building)
+        {
+            buildings.Add(building);
+        }
+
+
+
     }
 }
