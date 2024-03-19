4e9e496da8d005f671666db31a72bac684715e14
Kyal Cantwell <balthizar01@live.com>
2024-03-19 15:08:04 -0400

    Yep

---
+++ highsail-text-rpg/Models/Monster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace highsail_text_rpg.Models
+{
+    internal class Monster
+    {
+    }
+}
